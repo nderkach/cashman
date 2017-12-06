b4914c674586fb099c8c88c50a9b6b4141a76473
Nikolay Derkach <nderk@me.com>
2017-12-06 18:33:24 +0300

    Add JWT authentication

--- cashman/index.py
+++ cashman/index.py
@@ -1,10 +1,18 @@
 from flask import Flask, jsonify, request
+from flask_jwt_simple import (
+    JWTManager, jwt_required, create_jwt, get_jwt_identity
+)
+import os
 
 from cashman.model.expense import Expense, ExpenseSchema
 from cashman.model.income import Income, IncomeSchema
 from cashman.model.transaction_type import TransactionType
 
 app = Flask(__name__)
+
+# Setup the Flask-JWT-Simple extension
+app.config['JWT_SECRET_KEY'] = os.environ.get("JWT_SECRET", "supersecret")
+jwt = JWTManager(app)
 
 transactions = [
     Income('Salary', 5000),
@@ -14,7 +22,30 @@ transactions = [
 ]
 
 
+@app.route('/login', methods=['POST'])
+def login():
+    if not request.is_json:
+        return jsonify({"msg": "Missing JSON in request"}), 400
+
+    params = request.get_json()
+    username = params.get('username', None)
+    password = params.get('password', None)
+
+    if not username:
+        return jsonify({"msg": "Missing username parameter"}), 400
+    if not password:
+        return jsonify({"msg": "Missing password parameter"}), 400
+
+    if username != 'test' or password != 'test':
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    # Identity can be any data that is json serializable
+    ret = {'jwt': create_jwt(identity=username)}
+    return jsonify(ret), 200
+
+
 @app.route('/incomes')
+@jwt_required
 def get_incomes():
     schema = IncomeSchema(many=True)
     incomes = schema.dump(
@@ -23,7 +54,9 @@ def get_incomes():
     return jsonify(incomes.data)
 
 
+
 @app.route('/incomes', methods=['POST'])
+@jwt_required
 def add_income():
     income = IncomeSchema().load(request.get_json())
     transactions.append(income.data)
@@ -31,6 +64,7 @@ def add_income():
 
 
 @app.route('/expenses')
+@jwt_required
 def get_expenses():
     schema = ExpenseSchema(many=True)
     expenses = schema.dump(
@@ -40,6 +74,7 @@ def get_expenses():
 
 
 @app.route('/expenses', methods=['POST'])
+@jwt_required
 def add_expense():
     expense = ExpenseSchema().load(request.get_json())
     transactions.append(expense.data)
